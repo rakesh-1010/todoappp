218d428a62187c2c9ae8bef57a8e3f06d05818d7
rakesh <sendittorakesh@gmail.com>
2018-01-16 13:49:02 +0530

    UI inhancement

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,7 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_task,only: [:new,:create,:update,:edit]
+  before_action :set_task,only: [:new,:create]
+  before_action :set_comment,only: [:edit,:update,:destroy]
 
   def create
     @comment = @task.comments.build(comments_params)
@@ -15,13 +16,18 @@ class CommentsController < ApplicationController
 
   end
 
-  def delete
-
+  def destroy
+    @task = @comment.task
+    @comment.destroy
   end
 
   private
     def set_task
       @task = Task.find_by(id: params[:comment][:task_id])
+    end
+
+    def set_comment
+      @comment = Comment.find(params[:id])
     end
 
     def comments_params
