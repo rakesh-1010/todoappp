5f8056b64deac9ac0c989cb9cef90090406b42e5
rakesh <sendittorakesh@gmail.com>
2018-01-30 12:21:03 +0530

    Cleaned up code in model

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,13 +1,7 @@
 class Tag < ActiveRecord::Base
+
   belongs_to :user
   belongs_to :task
 
-
-  def self.add_tag(user_id,task_id)
-    Taging.create(user_id: user_id,task_id: task_id)
-  end
-
-  def remove_tag
-    self.destroy
-  end
 end
+
